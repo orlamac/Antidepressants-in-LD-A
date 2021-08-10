a28e956a56e165fc0f90ce356e507a439f054ec6
orlamac <orlamolloy@hotmail.com>
2021-08-10 16:29:39 +0000

    initial code for SSRIs

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,13 +1,51 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv, filter_codes_by_category, combine_codelists, Measure  # NOQA
 
+# Import codelists from codelist.py folder
+from codelists import SSRI_codes, LD_codes, Autism_codes
+
+# Define Study time variables
+from datetime import datetime
+end_date = datetime.today().strftime('%Y-%m-%d')
+
+# Define Study population and variables
 
 study = StudyDefinition(
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "date": {"earliest": "2019-01-01", "latest": end_date},
         "rate": "uniform",
         "incidence": 0.5,
     },
     population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+        "2019-02-01", end_date
     ),
+)
+# Set index date
+index_date = "2019-01-01"
+
+# Define Medication variables
+
+# Patients who are taking SSRIs
+SSRI_cohort = patients.with_these_medications(
+    SSRI_codes,
+    on_or_before=index_date,
+    returning="binary_flag",
+    return_expectations={"incidence": 0.5}
+)
+
+# Define patient populations
+
+# Patients with a learning disability
+learning_disability = patients.with_these_clinical_events(
+    LD_codes,
+    on_or_before=index_date,
+    returning="binary_flag",
+    return_expectations={"incidence": 0.5}
+)
+
+# Patients with Autism
+autism = patients.with_these_clinical_events(
+    Autism_codes,
+    on_or_before=index_date,
+    returning="binary_flag",
+    return_expectations={"incidence": 0.5},
 )
